364f14288b9276082639154047a2b1825cc4aa40
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-04-11 14:50:07 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+// import "./App.css";
 
-import ResultContainerPlugin from './ResultContainerPlugin.jsx';
-import Html5QrcodePlugin from './Html5QrCodePlugin';
+import ResultContainerPlugin from "./ResultContainerPlugin";
+import Html5QrcodePlugin from "./Html5QrCodePlugin";
 
 const App = (props) => {
-    const [decodedResults, setDecodedResults] = useState([]);
-    const onNewScanResult = (decodedText, decodedResult) => {
-        console.log("App [result]", decodedResult);
-        setDecodedResults(prev => [...prev, decodedResult]);
-    };
+  const [decodedResults, setDecodedResults] = useState([]);
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log("App [result]", decodedResult);
+    setDecodedResults((prev) => [...prev, decodedResult]);
+  };
 
-    return (
-        <div className="App">
+  return (
+    <div className="App">
+            <section className="App-section">
+                <div className="App-section-title"> Html5-qrcode React demo</div>
+                <br />
+                <br />
+                <br />
                 <Html5QrcodePlugin
                     fps={10}
                     qrbox={250}
@@ -20,10 +25,9 @@ const App = (props) => {
                     qrCodeSuccessCallback={onNewScanResult}
                 />
                 <ResultContainerPlugin results={decodedResults} />
-               
-            
+            </section>
         </div>
-    );
+  );
 };
 
 export default App;
